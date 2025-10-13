95a75e3c57a33947b0769a578efb8fe12eaf17d2
SekretySnu <youngninja5724@gmail.com>
2025-10-13 22:48:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ loader.load(modelUrl, (gltf) => {
     model.position.sub(center);
 
     // Docelowa wysokość modelu w scenie (możesz dopasować rozmiar)
-    const targetHeight = 2.0; 
+    const targetHeight = 1.25; 
     if (size.y > 0) {
         const scale = targetHeight / size.y;
         model.scale.setScalar(scale);
